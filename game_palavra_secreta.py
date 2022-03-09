963de5efa1432fc55b74ae480777a9a5f9c54820
Antonio Marcos Pereira <51891749+antoniomarcosap@users.noreply.github.com>
2022-03-09 17:45:31 -0300

    Update Palavra Secreta

--- game_palavra_secreta.py
+++ game_palavra_secreta.py
@@ -1,19 +1,19 @@
-secreto = "perfume"
-digitadas = []
-jogadas = 3
+secreto = "perfume"         # Palavra secreta
+digitadas = []              
+jogadas = 3                 # Número de erros permitidos
 
 while True:
     if jogadas <= 0:
-        print("Você perdeu!!! :(")
+        print("Você perdeu!!! :(")      # Quando zerar o número de erros o jogo é encerrado
         break
         
     letra = input("Digite uma letra: ")
 
-    if len(letra) > 1 or letra != str(letra) or letra == "":
+    if len(letra) > 1 or letra != str(letra) or letra == "":        # Limita o jogo ao uso de apenas letras e somente uma por vez
         print("Opa!!! Você só pode digitar uma letra.")
         continue
 
-    digitadas.append(letra)
+    digitadas.append(letra)             # Letra jogada é adicionada à lista "digitadas"
 
     if letra in secreto:
         print(f"Muito bem!!! A letra {letra} faz parte da palavra.")
@@ -21,20 +21,22 @@ while True:
         print(f"Sinto muito, mas o caratere '{letra}' não faz parte da palavra secreta")
         digitadas.pop()
 
+    # Loop exibe a letra jogada quando faz parte da palavra e oculta as demais letras
     secreto_temporario = ""
     for letra_secreta in secreto:
         if letra_secreta in digitadas:
             secreto_temporario += letra_secreta
         else:
             secreto_temporario += "*"
-
+    
+    # Caso todas as palavras sejam encontrada, encerra o jogo. Caso contrário, mostra a situação atual de letras descobertas
     if secreto_temporario == secreto:
         print(f"Parabéns!!! Você achou a palavra '{secreto_temporario}'.")
         break
     else:
         print(f"A palavra secreta está assim: {secreto_temporario}")
 
-    if letra not in secreto:
+    if letra not in secreto:    # Se a letra não faz parte da palavra secreta, é decrementado uma jogada.
         jogadas -= 1
 
     print(f"Você ainda tem {jogadas} jogadas.\n")
